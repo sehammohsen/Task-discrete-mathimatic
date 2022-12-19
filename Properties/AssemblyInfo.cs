71ee624a86fede0b69d9873d9ee439c928c76ad4
sehammohsen <121043286+sehammohsen@users.noreply.github.com>
2022-12-19 17:47:16 +0200

    Add files via upload

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("prime number")]
+[assembly: AssemblyTitle("perfect number")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("prime number")]
+[assembly: AssemblyProduct("perfect number")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cf4845bf-669d-40ce-b5db-98b4a7be837f")]
+[assembly: Guid("c01e149e-7e9e-421a-b06e-bfb11ff6e308")]
 
 // Version information for an assembly consists of the following four values:
 //
